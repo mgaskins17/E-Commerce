614f2eee7713d6d34ee2cd6739f28c955f788e8d
mgaskins17 <mattgaskins77@gmail.com>
2023-01-13 19:26:11 -0700

    built out index relations for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,37 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'reader_id'
+});
 
 // Categories have many Products
+Product.hasMany(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'productTags'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'ProductTag',
+    unique: false
+  },
+  as: 'tagProducts'
+});
+
+
+
 
 module.exports = {
   Product,
