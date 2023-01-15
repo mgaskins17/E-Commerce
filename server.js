702d36edcac62d73f7e70cef39436362ff7aba09
mgaskins17 <mattgaskins77@gmail.com>
2023-01-14 18:05:28 -0700

    committing before submission

--- server.js
+++ server.js
@@ -19,8 +19,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
 })
-
-
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
